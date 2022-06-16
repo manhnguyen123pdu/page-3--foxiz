dc449999921dd9383108fcfb1877ebed2eb50435
your name <manhnguyen.tla@gmail.com>
2022-06-16 22:03:26 +0700

    s

--- trang3.js
+++ trang3.js
@@ -1,20 +1,22 @@
 var sun = document.querySelectorAll('.sun')
 var moon = document.querySelectorAll('.moon')
-var background = document.querySelector('.background')
+var background = document.querySelectorAll('.background')
 var logodark = document.querySelectorAll('.logodark')
 var taga = document.getElementsByTagName('a')
 var nav__overlay = document.querySelectorAll('.nav__overlay')
 var nav2btn = document.querySelector('.nav2btn')
 var overmenu2__contents = document.querySelector('.overmenu2__contents ')
-var nav2 = document.querySelector('.nav2t ')
+var nav2 = document.querySelector('.overmenu2__contents ')
 for (m = 0; m < moon.length; m++) {
     moon[m].onclick = function () {
-        background.classList.add('background--black')
+        for(b=0;b<background.length;b++){
+            background[b].classList.add('background--black')
+        }
         for (n = 0; n < moon.length; n++) {
             moon[n].classList.add('color__icon')
             sun[n].classList.remove('color__icon')
         }
-        for (l = 0; l < 3; l++) {
+        for (l = 0; l < logodark.length; l++) {
             logodark[l].classList.add('vissible')
         }
         for (i = 0; i < taga.length; i++) {
@@ -23,18 +25,21 @@ for (m = 0; m < moon.length; m++) {
         for (k = 0; k < nav__overlay.length; k++) {
             nav__overlay[k].classList.add('background--black')
         }
+        nav2.classList.add('background--black')
 
     }
 }
 
 for (s = 0; s < sun.length; s++) {
     sun[s].onclick = function () {
-        background.classList.remove('background--black')
+        for(b=0;b<background.length;b++){
+            background[b].classList.remove('background--black')
+        }
         for (n = 0; n < moon.length; n++) {
             sun[n].classList.add('color__icon')
             moon[n].classList.remove('color__icon')
         }
-        for (l = 0; l < 3; l++) {
+        for (l = 0; l < logodark.length; l++) {
             logodark[l].classList.remove('vissible')
         }
         for (i = 0; i < taga.length; i++) {
@@ -43,7 +48,7 @@ for (s = 0; s < sun.length; s++) {
         for (k = 0; k < nav__overlay.length; k++) {
             nav__overlay[k].classList.remove('background--black')
         }
-
+        nav2.classList.remove('background--black')
     }
 }
 nav2btn.onclick = function () {
